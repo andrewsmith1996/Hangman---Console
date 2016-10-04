c221fcd66fa62e4b1c8cf518f3f0bed095a6b936
Andrew <andrewsmith96@Hotmail.co.uk>
2016-10-04 10:13:57 +0100

    Added new lives feature

--- main.cpp
+++ main.cpp
@@ -11,39 +11,63 @@
 #include <stdio.h>
 #include <vector>
 #include <cstring>
+#include <cctype>
+
 
 
 using namespace std;
 
 
-
-
 string getWord(){
-    cout << "Player One Enter Word\n";
-    string userWord;
     
+    //Get Word from User
+    cout << "Player One Enter Word\n";
+   
+    string userWord;
     cin >> userWord;
+    
+    //Convert the word to uppercase
+    transform(userWord.begin(), userWord.end(),userWord.begin(), ::toupper);
     
     return userWord;
 }
 
 char getGuess(){
+    
+    //Get the guess from the user
     cout << "Take a Guess!\n";
     
     char guess;
     
     cin >> guess;
     
-    return guess;
+    //Return the uppercase version of the guess
+    return toupper(guess);
 }
 
-int checkIfInWord(string userWordList, char guess){
+bool checkIfInWord(string userWordList, char guess){
+    bool inWord = false;
+    
+    //Check each character in the string to see if the guess is present
+    for(int count = 0; count < userWordList.size(); count++){
+        if(userWordList[count] == guess){
+            inWord = true;
+
+        }
+    }
+    
+    return inWord;
+}
+
+
+int checkWhereInWord(string userWordList, char guess){
     int position;
     
+    //Check each character in string for the guess
     for(int count = 0; count < userWordList.size(); count++){
         if(userWordList[count] == guess){
             position = count;
-
+            
         }
     }
     
@@ -53,6 +77,8 @@ int checkIfInWord(string userWordList, char guess){
                           
 string hideWord(string userWord){
     string hiddenWord = "";
+    
+    //Create a new string of stars
     for(int count = 0; count < userWord.size(); count++){
         hiddenWord+= "*";
     }
@@ -60,19 +86,24 @@ string hideWord(string userWord){
     return hiddenWord;
 }
 
-bool checkIfGameWon(string word){
-    bool won = false;
-    return 0;
+
+int checkIfUnmasked(string hiddenWord){
+    
+    //Count number of stars to see if string has been unmasked
+    int numberOfStars = count(hiddenWord.begin(), hiddenWord.end(), '*');
+    
+    return numberOfStars;
 }
 
 int main() {
     
     bool gameWon = false;
+    int lives = 5;
     
     //Get the word
-    
     string userWordOriginal = getWord();
     
+    //Mask the word
     string hiddenWord = hideWord(userWordOriginal);
 
     
@@ -90,23 +121,50 @@ int main() {
     
         //Find number of occurences
         int occurences = count(userWordOriginal.begin(), userWordOriginal.end(), guess);
+        
     
-  
-        for(int count = 0; count < occurences; count++){
-            int position = checkIfInWord(userWordList, guess);
-           
-
-            if(position){
+        //Actual Game Loop
+        if(occurences != 0){
+            for(int count = 0; count < occurences; count++){
+        
+                //Check where the guess is in the word
+                int position = checkWhereInWord(userWordList, guess);
+                
+                //Edit the masked string
                 hiddenWord[position] = userWordOriginal[position];
                 userWordList[position] = '*';
-            } else{
-                lives--;            }
+                
+                //Check if word completely unmasked
+                int numberStars = checkIfUnmasked(hiddenWord);
+               
+                if(numberStars == 0){
+                    gameWon = true;
+                }
+
+            }
+            
+        } else{
+            //If wrong then decrease the lives of the player
+            lives--;
+            
+            //If no lives remaining then the game is over
+            if(lives == 0){
+                gameWon = true;
+            }
+
         }
     
-        cout << hiddenWord;
+        //Print the word to the screen
+        cout << hiddenWord << endl;
     
     }
     
+    //Check how many lives remaining
+    if(lives == 0){
+        cout << "You ran out of lives!\nPlayer 2 Wins!\n";
+    } else{
+        cout << "You guessed the word!\nPlayer 1 Wins!\n";
+    }
     
     return 0;
 }
